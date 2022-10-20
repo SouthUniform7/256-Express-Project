fcd3d776ae5c0bd8d40c7ef3381edb743bd76650
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-10-20 17:42:25 -0400

    search page for lab 9

---
+++ public/JS/load_json.js
@@ -0,0 +1,29 @@
+/*$(document).ready(function () {
+    $.get("data/images.json", updatesImage);
+}); */
+let i = 0;
+function loadRandomImage(){
+    $.get("db/images.json", (data) => {
+        i += 1;
+        const randomIndex = Math.floor(Math.random() * 6)
+        const imageInfo = data[randomIndex]; 
+        const span = $("<span>").text(imageInfo.title)
+        const img = $("<img/>").attr("src", imageInfo.image);
+        const div = $("<div></div>").append(span).append($("<br>")).append(img)
+        if (i%2 == 1){
+            $("#col1").append(div)
+        } else{
+            $("#col2").append(div)
+        }
+    });
+}
+
+$(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        console.log(value);
+        $("#images div").filter( function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
