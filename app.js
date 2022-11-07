3e03245efd5602eb083b3fb1374ae24c41f630e2
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-11-07 00:09:26 -0500

    location tab

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-var createError = require('http-errors');
+var createError = require("http-errors");
 
-var express = require('express');
+var express = require("express");
 
-var path = require('path');
+var path = require("path");
 
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 
-var logger = require('morgan');
+var logger = require("morgan");
 
- 
+var indexRouter = require("./routes/index");
 
-var indexRouter = require('./routes/index');
-
-var usersRouter = require('./routes/users');
-
- 
+var usersRouter = require("./routes/users");
 
 var app = express();
 
- 
-
 var port = 3001;
-
-
 
 // view engine setup
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
- 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(express.json());
 
@@ -40,91 +30,98 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use(
+  "/build/",
+  express.static(path.join(__dirname, "node_modules/three/build"))
+);
 
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+app.use(
+  "/jsm/",
+  express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
+);
 
- 
-
-var trips = require('./public/db/movieList.json');
-
- 
-
-app.get('/getList', function(req, res){
-
-    res.setHeader('Content-Type', 'application/json');
-
-    res.end(JSON.stringify(trips));
-
-   });
-
- 
-
-   app.post('/setRating', function(req, res){
-
-       
-
-      //  trips[req.body.idx].rating = req.body.rating;
-
-      for (let i = 0; i < trips.length; i++) {
-
-        if (trips[i].title === req.body.name) {
-
-          trips[i].rating = req.body.rating;
-
-          break;
-
-        }
-
-      }
-
-       res.setHeader('Content-Type', 'application/json');
-
-       res.end(JSON.stringify(trips));
-
- });
+var trips = require("./public/db/movieList.json");
 
 
+app.get("/getList", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(JSON.stringify(trips));
+});
+
+app.post("/setRating", function (req, res) {
+  //  trips[req.body.idx].rating = req.body.rating;
+
+  for (let i = 0; i < trips.length; i++) {
+    if (trips[i].title === req.body.name) {
+      trips[i].rating = req.body.rating;
+
+      break;
+    }
+  }
+
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(JSON.stringify(trips));
+});
+
+var addresses = require("./public/db/locations.json");
+
+app.get("/getAddress", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(JSON.stringify(addresses));
+});
+
+app.post("/setAddress", function (req, res) {
+  //  trips[req.body.idx].rating = req.body.rating;
+
+  for (let i = 0; i < addresses.length; i++) {
+    if (addresses[i].username === req.body.username) {
+      addresses[i].address = req.body.address;
+      addresses[i].line2 = req.body.line2;
+      addresses[i].city = req.body.city;
+      addresses[i].state = req.body.state;
+      addresses[i].zip = req.body.zip;
+
+      break;
+    }
+  }
+
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(JSON.stringify(addresses));
+});
 
 
-app.use('/', indexRouter);
 
-app.use('/users', usersRouter);
 
- 
+app.use("/", indexRouter);
+
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 
-app.use(function(req, res, next) {
-
+app.use(function (req, res, next) {
   next(createError(404));
-
 });
-
- 
 
 // error handler
 
-app.use(function(err, req, res, next) {
-
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
 
   res.locals.message = err.message;
 
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
- 
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
 
   res.status(err.status || 500);
 
-  res.render('error');
-
+  res.render("error");
 });
-
- 
 
 module.exports = app;
--- public/JS/content.js
+++ public/JS/content.js
@@ -1,15 +1,11 @@
 function loadData(tripInfo) {
+  console.log(tripInfo);
 
-    console.log(tripInfo);
+  // var div = $('<div></div>').append(JSON.stringify(tripInfo));
 
- 
+  $("#content").empty();
 
-    // var div = $('<div></div>').append(JSON.stringify(tripInfo));
-
-$("#content").empty();
-
-for (let i = 0; i < tripInfo.length; i++) {
-
+  for (let i = 0; i < tripInfo.length; i++) {
     const oneTrip = tripInfo[i];
 
     const span = $("<span></span>").append(" rating: " + oneTrip.rating);
@@ -17,33 +13,62 @@ for (let i = 0; i < tripInfo.length; i++) {
     const paragraph = $("<p></p>").append(oneTrip.title).append(span);
 
     $("#content").append(paragraph);
-
+  }
 }
-
- 
-
-}
-
- 
 
 function getInfo() {
-
-    $.get("/getList", loadData);
-
+  $.get("/getList", loadData);
 }
-
- 
 
 function setData() {
+  const name = $("#updateTrip")[0].value;
 
-    const name = $('#updateTrip') [0].value;
+  const rating = $("#updateRating")[0].value;
 
-    const rating = $('#updateRating') [0].value;
+  console.log("name and rating " + name + " " + rating);
 
- 
-
-    console.log('name and rating ' + name + " " + rating)
-
-    $.post("/setRating", { name, rating }, loadData);
-
+  $.post("/setRating", { name, rating }, loadData);
 }
+
+
+
+
+function loadAddress(addy) {
+  console.log(addy);
+
+  // var div = $('<div></div>').append(JSON.stringify(tripInfo));
+
+  $("#content").empty();
+
+  for (let i = 0; i < addy.length; i++) {
+    
+    const loc = addy[i];
+
+    const span = $("<span></span>");
+    span.append("Username: " + loc.username).append($("<br>"))
+    span.append("Address: " + loc.address).append($("<br>"));
+    span.append("Line 2: " + loc.line2).append($("<br>"))
+    span.append("City: " + loc.city).append($("<br>"))
+    span.append("State: " + loc.state).append($("<br>"))
+    span.append("ZIP Code: " + loc.zip).append($("<br>"))
+
+    $("#content").append(span);
+  }
+}
+
+function getAddress() {
+  $.get("/getAddress", loadAddress);
+}
+
+function setAddress() {
+  const username = $("#username")[0].value;
+  const address = $("#address")[0].value;
+  const line2 = $("#line2")[0].value;
+  const city = $("#city")[0].value;
+  const state = $("#state option:selected").text();
+  console.log(state);
+  const zip = $("#zip")[0].value;
+
+  $.post("/setAddress", { username, address, line2, city, state, zip }, loadAddress);
+}
+
